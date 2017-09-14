6e1bea3bd4bf105a08b458bc920a8e56e451f681
Gilberto Méndez <gilberto.mendez.santiz@gmail.com>
2017-09-14 17:45:15 -0500

    project created and migrations were created

---
+++ database/migrations/2017_09_14_223225_create_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description',200);
+            $table->float('price',8,2);
+            $table->string('quantity');
+            $table->timestamps();
+        });
+        Schema::create('product_sale', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantity');
+            $table->integer('sale_id')->unsigned();
+            $table->foreign('sale_id')->references('id')->on('sales');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
